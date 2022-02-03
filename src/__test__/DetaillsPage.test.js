02e93d86f08ab96e4dc17f041224d3b76a979d6e
Tumworobere <annaht.annah@gmail.com>
2022-02-04 00:00:46 +0300

    Update components

--- src/__test__/DetaillsPage.test.js
+++ src/__test__/DetaillsPage.test.js
@@ -3,12 +3,12 @@ import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../Redux/configureStore';
-import HomePage from '../Components/HomePage';
+import DetailsPage from '../Components/HomePage';
 
 describe('Render components', () => {
-  it('render HomePage', () => {
+  test('render HomePage', () => {
     const tree = renderer
-      .create(<Router><Provider store={store}><HomePage /></Provider></Router>)
+      .create(<Router><Provider store={store}><DetailsPage /></Provider></Router>)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
