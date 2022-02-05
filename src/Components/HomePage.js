e066050f520a3b42f31e4dac55c5d197f5c409be
Tumworobere <annaht.annah@gmail.com>
2022-02-05 10:50:41 +0300

    Update files

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -47,7 +47,7 @@ const HomePage = () => {
   return (
     <div className="main-c">
       <div className="country-heading">
-        <h2 className="line-1 anim-typewriter">Covid is real and it kills, Kindly follow  SOPS </h2>
+        <h2 className="line-1 anim-typewriter">Active Corona Cases </h2>
       </div>
 
       <input className="search" type="text" id="input-country" placeholder="Country name.." onKeyDown={searchCountry} />
